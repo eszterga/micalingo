e50375857f60170b82a74a05f62d4689c1d28b73
eszterga <esztergalyos.ildiko@gmail.com>
2026-06-30 21:46:00 +0200

    as of google log in implemented

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,21 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/micalingo/',
   server: {
     port: 5173,
-    strictPort: true
+    strictPort: false
+  },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
