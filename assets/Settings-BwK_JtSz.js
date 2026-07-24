eda254827eae2339b8f83bfabc49bed9baf01f9f
Ildiko Esztergalyos <esztergalyos.ildiko@gmail.com>
2026-07-24 14:00:35 +0200

    Updates

--- assets/Settings-BwK_JtSz.js
+++ assets/Settings-BwK_JtSz.js
@@ -1,4 +1,4 @@
-import{r as n,j as e,L as T,p as k,A as B,q as O}from"./vendor-BZn69qKb.js";import{a as $,u as q,d as C}from"./index-DBpJAz8J.js";import{u as R,b as I}from"./firestore-dcGQIvOz.js";const J=()=>e.jsxs(e.Fragment,{children:[e.jsx("style",{children:`
+import{r as n,j as e,L as T,p as k,A as B,q as O}from"./vendor-BZn69qKb.js";import{a as $,u as q,d as C}from"./index-CUfLCTZu.js";import{u as R,b as I}from"./firestore-B-0U8Gyu.js";const J=()=>e.jsxs(e.Fragment,{children:[e.jsx("style",{children:`
       @keyframes blob {
         0% { transform: translate(0px, 0px) scale(1); }
         33% { transform: translate(30px, -50px) scale(1.1); }
