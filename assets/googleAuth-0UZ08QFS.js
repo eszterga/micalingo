9407094ac99d4d1c04ab9ae8b68c56c94bbe4b76
eszterga <eszterga@users.noreply.github.com>
2026-07-28 09:12:52 +0000

    deploy: f2c9f60ffed08083334ab1fce5ee0dfe7e98439c

---
+++ assets/googleAuth-0UZ08QFS.js
@@ -0,0 +1,1 @@
+import{C as o,M as a,N as i,P as r}from"./vendor-CsELMyAt.js";import{b as e}from"./index-p7QDeUwb.js";async function p(){if(o.isNativePlatform()){await a.signInWithGoogle();return}const t=new i;return t.setCustomParameters({prompt:"select_account"}),r(e,t)}export{p as s};
